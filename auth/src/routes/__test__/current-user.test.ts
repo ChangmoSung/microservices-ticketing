e16003a772704a998a53219386215dd3fe362211
ChangmoSung <changmosung0914@gmail.com>
2021-09-18 17:11:58 -0400

    adding jest tests for authentication

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("responds with details about the current user", async () => {
+  const authResponse = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com", password: "123" })
+    .expect(201);
+  const cookie = authResponse.get("Set-Cookie");
+
+  const response = await request(app)
+    .get("/api/users/currentUser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual("test@gmail.com");
+});
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -7,3 +7,50 @@ it("returns 201 on successful signup", async () => {
     .send({ email: "test@gmail.com", password: "123" })
     .expect(201);
 });
+
+it("returns 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "testEmail", password: "123" })
+    .expect(400);
+});
+
+it("returns 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com", password: "" })
+    .expect(400);
+});
+
+it("returns 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com" })
+    .expect(400);
+
+  return request(app)
+    .post("/api/users/signup")
+    .send({ password: "123" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com", password: "123" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com", password: "123" })
+    .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@gmail.com", password: "123" })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
