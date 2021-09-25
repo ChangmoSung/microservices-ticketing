af665eaa6ac3f964301ed726efb7f5a01e40a50e
ChangmoSung <changmosung0914@gmail.com>
2021-09-25 16:51:11 -0400

    onSuccess callback and destructuring

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,22 +1,30 @@
 import axios from "axios";
 import { useState } from "react";
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null);
 
-      const response = await axios[method](url, body);
-      return response.data;
-    } catch (error) {
+      const { data } = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(data);
+      }
+      return data;
+    } catch ({
+      response: {
+        data: { errors },
+      },
+    }) {
       setErrors(
         <div className="alert alert-danger">
           <h4>Something went wrong</h4>
           <ul className="my-0">
-            {error.response.data.errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
+            {errors.map(({ message }) => (
+              <li key={message}>{message}</li>
             ))}
           </ul>
         </div>
