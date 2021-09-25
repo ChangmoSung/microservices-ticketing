59adfd0da255f8c8288eaca5b25885cded0b7689
ChangmoSung <changmosung0914@gmail.com>
2021-09-25 17:46:53 -0400

    server side props and reusable buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
-const Landing = () => {
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
 
-export default Landing;
+export const getServerSideProps = async (context) => {
+  const client = buildClient(context);
+  const { data: currentUser } = await client.get("/api/users/currentUser");
+  return { props: { currentUser } };
+};
+
+export default LandingPage;
