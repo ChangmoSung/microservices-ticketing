2611eba59ce5c523136d3e6bfc59099e908f86bf
ChangmoSung <changmosung0914@gmail.com>
2021-12-13 05:13:25 +0900

    publishing and subscribing events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,16 +1,40 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
-  const { JWT_KEY = "", MONGO_URI = "" } = process.env;
+  const {
+    JWT_KEY = "",
+    MONGO_URI = "",
+    NATS_CLIENT_ID = "",
+    NATS_URL = "",
+    NATS_CLUSTER_ID = "",
+  } = process.env;
   if (!JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
   if (!MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
+  if (!NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  if (!NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
 
   try {
+    await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL);
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
